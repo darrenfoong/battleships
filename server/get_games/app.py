0116d02001b57ca2d305d7ae8a72d9fb1b13e6cf
Darren Foong <darrenfoong@gmail.com>
2020-09-06 14:18:59 +0800

    Initial implementation of get_games

--- server/get_games/app.py
+++ server/get_games/app.py
@@ -1,42 +1,31 @@
+import boto3
 import json
+import jsonpickle
+import uuid
+import os
 
-# import requests
+from common import Game
+
+if os.getenv("AWS_SAM_LOCAL", ""):
+    client = boto3.client("dynamodb",
+                          endpoint_url="http://172.17.0.2:8000")
+    BATTLESHIPS_TABLE = "battleships-table"
+else:
+    client = boto3.client("dynamodb")
+    BATTLESHIPS_TABLE = os.environ["TABLE_NAME"]
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    games = list(map(lambda item: {
+        "id": item["id"]["S"],
+        "value": item["value"]["S"]
+    }, client.scan(
+        TableName=BATTLESHIPS_TABLE
+    )["Items"]))
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "games": games
         }),
     }
